f1c9c32b4de2366c27f398b6033289dfa3249df9
rabbani4u <rabbani.iu31@gmail.com>
2022-11-30 11:49:28 +0600

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
+// ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
+// var numbers = [3, 56, 2, 48, 5];
+
+//Map -Create a new array by doing something with each item in an array.
+
+// let newNumber = [];
+
+// numbers.forEach(function (x) {
+//   newNumber.push(x * 2);
+// });
+
+// const newNumber = numbers.map(function (x) {
+//   return x * 2;
+// });
+
+// console.log(newNumber);
+
+var numbers = [3, 56, 2, 48, 5];
+//Filter - Create a new array by keeping the items that return true.
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+
+//Find - find the first item that matches from an array.
+
+//FindIndex - find the index of the first item that matches.
