e8d6698009b19140177e3e1828f5d8e5e3248a63
rabbani4u <rabbani.iu31@gmail.com>
2022-11-30 12:23:58 +0600

    update

--- src/index.js
+++ src/index.js
@@ -18,24 +18,40 @@
 
 // console.log(newNumber);
 
-var numbers = [3, 56, 2, 48, 5];
+// var numbers = [3, 56, 2, 48, 5];
 //Filter - Create a new array by keeping the items that return true.
 
 // const newNumber = numbers.filter(function (num) {
 //   return num < 10;
 // });
 
-const newNumber = [];
+// const newNumber = [];
 
-numbers.forEach(function (num) {
-  if (num < 10) {
-    newNumber.push(num);
-  }
-});
+// numbers.forEach(function (num) {
+//   if (num < 10) {
+//     newNumber.push(num);
+//   }
+// });
 
-console.log(newNumber);
+// console.log(newNumber);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
+var numbers = [3, 56, 2, 48, 5];
+
+const newNumber = numbers.reduce(function (accumulator, currentNum) {
+  console.log("Accumulator = " + accumulator);
+  console.log("Current Number = " + currentNum);
+
+  return accumulator + currentNum;
+});
+console.log(newNumber);
+
+// let newNumber = 0;
+
+// numbers.forEach(function (currentNum) {
+//   newNumber += currentNum;
+// });
+// console.log(newNumber);
 
 //Find - find the first item that matches from an array.
 
